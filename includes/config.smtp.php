9318bfd11c4352873a4b3f02ef0b57fe625c39a1
Mulki <admiportfolio@gmail.com>
2026-06-10 17:31:36 +0700

    refactor: pisah SMTP config ke config.smtp.php agar ter-deploy via git

---
+++ includes/config.smtp.php
@@ -0,0 +1,7 @@
+<?php
+define('SMTP_HOST',      'mail.jurnalsinta.id');
+define('SMTP_PORT',      465);
+define('SMTP_USER',      'jurz2196');
+define('SMTP_PASS',      '8xtMS1QvQPrz19');
+define('SMTP_FROM',      'jurz2196@jurnalsinta.id');
+define('SMTP_FROM_NAME', 'Pusat Pengelolaan Jurnal LPPM Unsoed');
